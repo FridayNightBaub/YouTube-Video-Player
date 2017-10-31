93ab141384ae1db87ad83aa71694f8e024de7e9e
Bobby Fenn <bfenn138@gmail.com>
2017-10-30 22:26:11 -0600

    udemy ReactRedux initial commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+//functional component
+// const SearchBar = () => {
+//     return <input type="text"/>
+// }
+
+//class based component
+// class SearchBar extends React.Component{
+//     render(){
+//         return <input type="text"/>
+//     }
+// }
+
+//this is proper ES6 syntax. Add { Component } to the import and drop React. off extends
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' }
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    type="text" onChange={event => this.set.state({event.target.value})}/>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
